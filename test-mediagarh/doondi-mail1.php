244746fda348012e434e59de6d89611267e34086
Sakshimathur749 <mathursakshi749@gmail.com>
2026-04-01 15:05:16 +0530

    update

---
+++ test-mediagarh/doondi-mail1.php
@@ -0,0 +1,34 @@
+<?PHP
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "webleads@roots.ac.in";
+$mail->Password   = "Roots#123";
+
+$mail->IsHTML(true);
+$mail->AddAddress("doondeswar@gmail.com", "recipient-name");
+$mail->SetFrom("webleads@roots.ac.in", "from-name");
+//$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
+//$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
